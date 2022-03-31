a3d8d74fa176f69c9feece9e6de525583d40ef7d
Eunkyung Choi <lucidyul.choi@gmail.com>
2022-03-31 17:46:32 +0900

    searchByBirthday 기능 추가

--- AmigoPJT/main.cpp
+++ AmigoPJT/main.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
 
-int main() {
+int main ()
+{
+	std::string Path = "input_20_20.txt";
 
+	std::ifstream readFile (Path);
+	if (readFile.is_open ())
+	{
+		char line[200];
+		while (!readFile.eof ())
+		{
+			readFile.getline (line, sizeof (line));
+			std::cout << line << std::endl;
+		}
+		readFile.close ();
+	}
 	return 0;
 }
--- Test/test.cpp
+++ Test/test.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
+#include "../AmigoPJT/sch1.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+TEST(AmigoSCH1Test, TestByYear) {
+
+  EXPECT_EQ(SearchByBirthday("y","1977"), 1);
+  EXPECT_EQ (SearchByBirthday ("y", "1987"), 0);
+  EXPECT_EQ (SearchByBirthday ("y", "2000"), 0);
+  EXPECT_EQ (SearchByBirthday ("y", "2012"), 2);
+  EXPECT_EQ (SearchByBirthday ("y", "1923"), 0);
 }
