cb3b3f9b49213f1b4309fa1675987cb03d4e0320
su <cis3173@gmail.com>
2022-04-01 09:26:37 +0900

    add test 내 parset test 삭제

--- Test/add_test.cpp
+++ Test/add_test.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "../../Amigo/AmigoPJT/command.cpp"
-#include "../../Amigo/AmigoPJT/txt_parser.cpp"
 
 
 TEST(AmigoADDTest, ADD_Test) {
@@ -10,10 +9,4 @@ TEST(AmigoADDTest, ADD_Test) {
     EXPECT_EQ(2, Add("17112609", "FB NTAWR", "CL4", "010 - 5645 - 6122", "19861203", "PRO"));
     EXPECT_EQ(3, Add("18115040", "TTETHU HBO", "CL3", "010 - 4581 - 2050", "20080718", "ADV"));
     EXPECT_EQ(4, Add("88114052", "NQ LVARW", "CL4", "010 - 4528 - 3059", "19911021", "PRO"));
-}
-
-TEST(AmigoParserTest, Parser_Test) {
-
-    vector<Command> vect_result = ParserFromTxt("../../Amigo/AmigoPJT/input_20_20.txt");
-    EXPECT_EQ(40, vect_result.size());
 }
