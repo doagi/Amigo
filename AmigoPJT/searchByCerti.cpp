cff23c45d74678c7dc80e7225c65e1536c0db51e
Hyeongsung Cho <hscho0316@gmail.com>
2022-04-04 17:12:03 +0900

    [Refactoring] Change tabs to spaces to meet coding style rule

--- AmigoPJT/searchByCerti.cpp
+++ AmigoPJT/searchByCerti.cpp
@@ -2,15 +2,15 @@
 
 vector<unsigned int> searchByCerti(string target, unordered_map<unsigned int, Employee2> data)
 {
-	if (target != "ADV" && target != "PRO" && target != "EX")
-	{
-		throw std::out_of_range("Invalid CL.\n");
-	}
-	vector<unsigned int> result;
-	for (auto aData : data)
-	{
-		if (aData.second.certi != target) continue;
-		result.emplace_back(aData.first);
-	}
-	return result;
+    if (target != "ADV" && target != "PRO" && target != "EX")
+    {
+        throw std::out_of_range("Invalid CL.\n");
+    }
+    vector<unsigned int> result;
+    for (auto aData : data)
+    {
+        if (aData.second.certi != target) continue;
+        result.emplace_back(aData.first);
+    }
+    return result;
 }
--- AmigoPJT/searchByName.cpp
+++ AmigoPJT/searchByName.cpp
@@ -1,68 +1,68 @@
 #include "searchByName.h"
 
 vector <unsigned int> searchByName(string op2, string target, unordered_map<unsigned int, Employee2> data) {
-	if (target.length() > 13)
-	{
-		throw std::length_error("Too long name.\n");
-	}
-	vector<unsigned int> employees;
-	if (op2 == " ")
-	{
-		employees = searchByFullName(target, data);
-	}
-	else if (op2 == "-f")
-	{
-		employees = searchByFirstName(target, data);
-	}
-	else if (op2 == "-l")
-	{
-		employees = searchByLastName(target, data);
-	}
-	else
-	{
-		throw std::out_of_range("Invalid name option. Only -l or -f are available\n");
-	}
-	return employees;
+    if (target.length() > 13)
+    {
+        throw std::length_error("Too long name.\n");
+    }
+    vector<unsigned int> employees;
+    if (op2 == " ")
+    {
+        employees = searchByFullName(target, data);
+    }
+    else if (op2 == "-f")
+    {
+        employees = searchByFirstName(target, data);
+    }
+    else if (op2 == "-l")
+    {
+        employees = searchByLastName(target, data);
+    }
+    else
+    {
+        throw std::out_of_range("Invalid name option. Only -l or -f are available\n");
+    }
+    return employees;
 }
 
 vector<unsigned int> searchByFullName(string target, unordered_map<unsigned int, Employee2> data)
 {
-	vector<unsigned int> result;
-	for (auto aData : data)
-	{
-		if (aData.second.full_name != target) 
-		{
-			continue;
-		}
-		result.emplace_back(aData.first);
-	}
-	return result;
+    vector<unsigned int> result;
+    for (auto aData : data)
+    {
+        if (aData.second.full_name != target) 
+        {
+            continue;
+        }
+        result.emplace_back(aData.first);
+    }
+    return result;
 }
 
 vector<unsigned int> searchByFirstName(string target, unordered_map<unsigned int, Employee2> data)
 {
-	vector<unsigned int> result;
-	for (auto aData : data)
-	{
-		if (aData.second.first_name != target) 
-		{
-			continue;
-		}
-		result.emplace_back(aData.first);
-	}
-	return result;
+    vector<unsigned int> result;
+    for (auto aData : data)
+    {
+        if (aData.second.first_name != target) 
+        {
+            continue;
+        }
+        result.emplace_back(aData.first);
+    }
+    return result;
 }
 
 vector<unsigned int> searchByLastName(string target, unordered_map<unsigned int, Employee2> data)
 {
-	vector<unsigned int> result;
-	for (auto aData : data)
-	{
-		if (aData.second.last_name != target)
-		{
-			continue;
-		}
-		result.emplace_back(aData.first);
-	}
-	return result;
+    vector<unsigned int> result;
+    for (auto aData : data)
+    {
+        if (aData.second.last_name != target)
+        {
+            continue;
+        }
+        result.emplace_back(aData.first);
+    }
+    return result;
 }
