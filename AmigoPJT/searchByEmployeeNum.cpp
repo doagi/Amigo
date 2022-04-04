a5ce396fc47c727d7fb1883b3f329c5551d9d41c
Eunkyung Choi <lucidyul.choi@gmail.com>
2022-04-04 13:25:15 +0900

    [Refactoring] 1.  search.h에 search관련 함수들 모두 모아놓음. 2. client.h에 AmigoDatabase 클래스의 멤버 함수로 search관련 함수들 모두 추가 및 리턴값, 파라미터 수정

--- AmigoPJT/searchByEmployeeNum.cpp
+++ AmigoPJT/searchByEmployeeNum.cpp
@@ -3,10 +3,11 @@
 vector<unsigned int> searchByEmployeeNumber(string target, unordered_map<unsigned int, Employee2> data)
 {
 	vector<unsigned int> result;
-	for (auto aData : data)
+	unsigned int find_key = stoi(target.substr(0, 2)) > 68 ? stoi("19" + target) : stoi("20" + target);
+	auto aData = data.find(find_key);
+	if (aData != data.end())
 	{
-		if (aData.second.str_employee_num != target) continue;
-		result.emplace_back(aData.first);
+		result.emplace_back(find_key);
 	}
 	return result;
 }
