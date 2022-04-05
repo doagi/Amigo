8299ef05f7d102e3d1a86c3d92dd9c96db6829f8
Su <cis3173@gmail.com>
2022-04-05 13:15:09 +0900

    [Refactoring] SearchEngine 으로 추출 (#44)
    * [Refactoring] SearchEngine 으로 추출

--- AmigoPJT/amigoDatabase.h
+++ AmigoPJT/amigoDatabase.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "IDatabase.h"
+#include "amigoSearchEngine.h"
 
 class AmigoDatabase : public IDatabase
 {
@@ -14,46 +15,31 @@ public:
         supported_cmds["MOD"] = (int)(CommandType::ModCommand);
         supported_cmds["SCH"] = (int)(CommandType::SchCommand);
 #endif
+        amigo_search_engine = new AmigoSearchEngine(map_employees);
     }
 
     virtual string Query(Command cmd) override;
 
 private:
     int Add(string employee_num, string name, string cl, string phoneNum, string birthday, string certi);
+    vector<unsigned int> Search(string option, string column, string value);
     int Mod(const vector<unsigned int>& founds, string column, string value);
+    void ModifyColumnData(Employee& employee, const ModificationInfo& mod_info);
+    void Del(const vector<unsigned int>& deleteList);
+
 
     string GenerateCommandRecord(const std::string& command, const bool& detail_print, const vector<unsigned int>& targets);
     string GenerateDetailRecord(const std::string& command, const vector<unsigned int>& result);
 
-    void Del(const vector<unsigned int>& deleteList);
 
     /*
     vector<string> Mod(unordered_map<unsigned int, Employee>& map_employees,
         const vector<unsigned int>& found_data, const ModificationInfo& condition);
         */
 
-    void ModifyColumnData(Employee& employee, const ModificationInfo& mod_info);
-
-    //Search option에 따른 함수들 
-    vector<unsigned int> Search(string option2, string column, string target);
-    vector<unsigned int> SearchByType(string& target, SearchType type);
-    template <typename T> bool IsMatch(const std::pair<int, Employee>& employee, T target, SearchType type);
-    SearchType GetSearchType(string option, string column);
-    void isValidSearch(string target, SearchType type);
-    void checkIsValidEmployeeNumber(string target);
-    void checkIsValidName(string target);
-    void checkIsValidFirstName(string target);
-    void checkIsValidLastName(string target);
-    void checkIsValidCL(string target);
-    void checkIsValidPhoneNumber(string target);
-    void checkIsValidMiddlePhoneNumber(string target);
-    void checkIsValidLastPhoneNumber(string target);
-    void checkIsValidBirthday(string target);
-    void checkIsValidBirthdayYear(string target);
-    void checkIsValidBirthdayMonth(string target);
-    void checkIsValidBirthdayDay(string target);
-    void checkIsValidCerti(string target);
+        
     unordered_map<unsigned int, Employee> map_employees;
+    ISearchEngine* amigo_search_engine;
 };
 
 class AmigoDatabase2 : public AmigoDatabase
@@ -63,10 +49,9 @@ public:
     {
         // 기존 cmd는 AmigoDatabase::add() 요런식으로 처리.
 
-        return newFunc();
+        return Swap();
     }
 
 private:
-    // 기구현된 func들 옮기면 될듯.
-    string newFunc();
+    string Swap() { return ""; }
 };
